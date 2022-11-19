e179dffc1daac6378b6096ecdeda052a084c1a50
Christian Limberg <cnlimberg@gmail.com>
2022-11-19 20:21:48 +0900

    some changes

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -22,7 +22,7 @@ def attempt_download(file, repo='WongKinYiu/yolov7'):
 
     if not file.exists():
         try:
-            response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()  # github api
+            response = requests.get('https://api.github.com/repos/WongKinYiu/yolov7/releases/tags/v0.1').json()  # github api
             assets = [x['name'] for x in response['assets']]  # release assets
             tag = response['tag_name']  # i.e. 'v1.0'
         except:  # fallback plan
